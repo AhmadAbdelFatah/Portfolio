2bfc7f88d8a322bf0f0ed1790054e7e3eca3182f
Ahmed Abdelfattah <132746034+AhmadAbdelFatah@users.noreply.github.com>
2024-01-15 07:13:24 +0200

    upload the project

---
+++ script.js
@@ -0,0 +1,40 @@
+const changeNames = document.querySelector('.skills__list');
+changeNames.children[4].firstElementChild.innerText = 'Node js';
+changeNames.children[5].firstElementChild.textContent = 'Mongo DB';
+
+// ---
+const newSkill = `
+<li data-name="HTML" class="skills__column">
+  <h2 class="skills__title">TypeScript</h2>
+  <span class="skills__years">2 years</span>
+</li>
+`;
+let addSkill = document.querySelector('[data-name="Javascript"]');
+addSkill.insertAdjacentHTML('afterend',newSkill);
+
+// ---
+
+const remmoveElement = document.querySelector('.find-out-link');
+console.log(remmoveElement);
+remmoveElement.remove();
+
+// ---
+
+const imageWidth = document.querySelector('.bio__pic');
+console.log(imageWidth);
+imageWidth.style.width = '36rem'
+
+// ---
+
+const skillsList = document.querySelectorAll('.skills__column');
+console.log(skillsList);
+[...skillsList].map ((item) => {
+  item.addEventListener('click', () => {
+    const activeElement = document.querySelector('.underline');
+    if(activeElement){
+      activeElement.classList.remove('underline');
+    }
+    item.classList.add('underline');
+  })
+}
+);
